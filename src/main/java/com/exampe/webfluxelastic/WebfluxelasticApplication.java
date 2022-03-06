0619b3105a9af69fac4c6ada354ba2fb79cb1108
sanix <sanicksikani@gmail.com>
2022-03-06 19:56:45 +0300

    init

---
+++ src/main/java/com/exampe/webfluxelastic/WebfluxelasticApplication.java
@@ -0,0 +1,13 @@
+package com.exampe.webfluxelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxelasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxelasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampe/webfluxelastic/WebfluxelasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampe.webfluxelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxelasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
